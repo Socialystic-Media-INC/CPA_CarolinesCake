349c2c8138093de4a3fbfbb847b36111ccd82de5
hectorahow <hectorahow@gmail.com>
2024-11-20 11:06:56 -0400

    update

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 import matplotlib.pyplot as plt
@@ -13,17 +14,21 @@ import base64
 app = Flask(__name__)
 
 # Load the data
-data = pd.read_csv('cc_data.csv')
+data = pd.read_csv('cc_data_mth.csv')
 metrics = ['date', 'spend', 'purchases']
 data = data[metrics]
 
-# Model setup
+# model
+degree = 3
 X = data[['spend']]
 y = data['purchases']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+poly = PolynomialFeatures(degree=degree)
+X_poly_train = poly.fit_transform(X_train)
+X_poly_test = poly.transform(X_test)
 model = LinearRegression()
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+model.fit(X_poly_train, y_train)
+y_pred = model.predict(X_poly_test)
 mae = np.floor(mean_absolute_error(y_test, y_pred))
 
 @app.route('/')
@@ -33,18 +38,20 @@ def index():
 @app.route('/calculate', methods=['POST'])
 def calculate():
     spend_value = float(request.json['spend_value'])
-    predicted_purchases = int(np.ceil(model.predict([[spend_value]])[0]))
-    cpa1 = (spend_value / (predicted_purchases + mae))
-    cpa2 = (spend_value / (predicted_purchases - mae))
+    spend_value_poly = poly.transform([[spend_value]])
+    predicted_purchases = int(np.ceil(model.predict(spend_value_poly)[0]))
+    cpa1 = spend_value / (predicted_purchases + mae)
+    cpa2 = spend_value / (predicted_purchases - mae)
     
     # Visualization
-    plt.figure(figsize=(6,3.8))
+    plt.figure(figsize=(6, 3.8))
     sns.scatterplot(data=data, x='spend', y='purchases', label='Actual Data', color='blue')
-    plt.scatter(spend_value, predicted_purchases, color='red', s=100, label=f'Predicted for Spend=${spend_value}')
+    plt.scatter(spend_value, predicted_purchases, color='red', s=100, label=f'Predicted for Spend=${spend_value:,.0f}')
+    spend_range = np.linspace(data['spend'].min(), data['spend'].max(), 500).reshape(-1, 1)
     plt.title("Spend vs Purchases with Prediction")
     plt.xlabel("Spend ($)")
     plt.ylabel("Purchases")
-    plt.legend()
+    plt.legend(loc='upper left')
     
     # Convert plot to PNG image
     img = io.BytesIO()
@@ -52,11 +59,12 @@ def calculate():
     img.seek(0)
     plot_url = base64.b64encode(img.getvalue()).decode()
     
-    result_text = f'{predicted_purchases} ± {mae:.0f} purchases and a CPA between ${cpa1:.1f} - ${cpa2:.1f} are forecast for a spend of ${spend_value}'
-
+    # Result text
+    result_text = (f'{predicted_purchases} ± {mae:.0f} purchases and a CPA between '
+                   f'${cpa1:.1f} - ${cpa2:.1f} are forecast for a spend of ${spend_value:,.0f}')
+    
     return jsonify(result_text=result_text, plot_url=plot_url)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
-
